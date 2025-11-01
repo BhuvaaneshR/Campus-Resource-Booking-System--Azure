38c57cbcba5c8be1e1cb5b25206fb5e69ea46165
BhuvaaneshR <220701045@rajalakshmi.edu.in>
2025-11-01 17:47:39 +0530

    SAST - Update 32

---
+++ backend/jest.config.ts
@@ -0,0 +1,14 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+};
+export default config;
